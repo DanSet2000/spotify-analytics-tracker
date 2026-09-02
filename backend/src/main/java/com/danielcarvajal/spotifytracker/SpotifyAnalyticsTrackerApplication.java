a3c2a8c197504704623cf830df35aec4e80f66b9
Daniel Carvajal <danielcarva2000@hotmail.com>
2026-09-01 19:28:19 -0500

    Scaffold Backend

---
+++ backend/src/main/java/com/danielcarvajal/spotifytracker/SpotifyAnalyticsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.danielcarvajal.spotifytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAnalyticsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyAnalyticsTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielcarvajal/spotifytracker/SpotifyAnalyticsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielcarvajal.spotifytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAnalyticsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
